aa82f4452b191633ae01bde1f635ca1aa8aa9d12
FrostyBitzz <49996321+FrostyBitzz@users.noreply.github.com>
2019-04-26 04:00:15 -0400

    Add

---
+++ MART 380 Project4_1c/Assets/DoorOpen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public Animation BasementDoor;
+    public GameObject Door;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            if (Input.GetKey("e"))
+            {
+                BasementDoor.Play ();
+            }
+
+
+    }
+}
